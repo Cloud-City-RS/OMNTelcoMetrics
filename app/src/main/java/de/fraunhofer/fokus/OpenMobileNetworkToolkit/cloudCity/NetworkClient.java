a1a2fdd94c340cbd9a23f30f38e12f9ae892f1c4
RkShaRkz <darksidemetatron@gmail.com>
2024-11-08 14:26:09 +0100

    Fix retrofit
    Ok, so while the server URL **wasn't** hardcoded in the Retrofit client,
    the retrofit would get initialized only once (with the prefilled URL) and would
    simply ignore all the changed URLs passed to it because it already had an instance.
    
    Now, instead of just checking whether we already have an initialized client, we also check
    whether the new URL differs from what the last initialized client was registered to (it's base URL)
    and if they differ - we reinitialize a new Retrofit client.
    
    This commit (change) lets us change servers on the fly while the app is running.

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/cloudCity/NetworkClient.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/cloudCity/NetworkClient.java
@@ -1,31 +1,45 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit.cloudCity;
 
+import android.util.Log;
+
 import java.util.Locale;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkClient {
+    private static final String TAG = "NetworkClient";
 
     private static Retrofit retrofit;
+    private static String currentClientBaseUrl;
 
     /**
-     * Get retrofit client to be used for the communication. Creates singleton.
+     * Get retrofit client to be used for the communication. Creates and memoizes the client which will
+     * be reused as long as the URL stays the same - when the URL changes and differs from what the
+     * memoized ("singleton") client was initialized with - a new client gets initialized with the new base URL.
+     *
      * @return Instance of retrofit client to be used.
      */
     public static Retrofit getRetrofitClient(String url) {
-        if (retrofit == null) {
+        String baseUrl = String.format(Locale.US, "https://%s/", url);
+        // If we don't have a retrofit client, or the URL we're targetting is different
+        // than what the previous client was initialized with - build a new client;
+        // otherwise return the last initialized one
+        if (retrofit == null || !baseUrl.equalsIgnoreCase(currentClientBaseUrl)) {
             if (url == null || url.isEmpty()){
                 return null;
             }
 
-            String baseUrl = String.format(Locale.US, "https://%s/", url);
+            if (!url.equalsIgnoreCase(currentClientBaseUrl)) {
+                Log.w(TAG, "URL has changed from previous Retrofit client's base URL, instantiating new client...");
+            }
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
                     .client(CustomClient.getUnsafeOkHttpClient())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
+            currentClientBaseUrl = baseUrl;
         }
 
         return retrofit;

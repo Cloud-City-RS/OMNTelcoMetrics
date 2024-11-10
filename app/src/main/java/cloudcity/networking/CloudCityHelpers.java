f0907965e3c212639b6ba194558438d0000fadfb
RkShaRkz <darksidemetatron@gmail.com>
2024-11-10 17:44:33 +0100

    Fix the header conundrum
    So, apparently the header itself was being set in the ServerAPI interface,
    while the value of it was being sent as "Bearer "+token", so when we got rid
    of the "bearer" it was still being sent as "Authorization: "+token.
    
    Now the header is properly being set in the ServerAPI as X-MAC-Address and the value
    is being passed down as just the MAC address alone.
    
    Oh and the MAC listener/sniffer doesn't really pick up anything yet.
    
    But the requests pass fine:
    
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  --> POST https://staging.app.cloudcities.co/api/sensor-events
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  Content-Type: application/json
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  Content-Length: 170
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  Accept: application/json
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  X-MAC-Address: 00-B0-D0-63-C2-AA
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  {"sensor_events":[{"accuracy":0.0,"category":"LTE","cell_info":{"cellId":440,"eNodeBId":6},"speed":0.0,"lat":0.0,"lon":0.0,"values":{"rsrp":-105,"rsrq":-18,"rssnr":-1}}]}
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  --> END POST (170-byte body)
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  <-- 201 https://staging.app.cloudcities.co/api/sensor-events (211ms)
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  server: nginx
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  content-type: text/html; charset=UTF-8
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  x-powered-by: PHP/8.2.0
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  cache-control: no-cache, private
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  date: Sun, 10 Nov 2024 16:43:11 GMT
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  x-ratelimit-limit: 500
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  x-ratelimit-remaining: 494
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  phpdebugbar-id: X5c67e3caf5a65cc5414714fb5a7bab89
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  vary: Origin
    okhttp.OkHttpClient     de....okus.OpenMobileNetworkToolkit  I  <-- END HTTP (0-byte body)

--- app/src/main/java/cloudcity/networking/CloudCityHelpers.java
+++ app/src/main/java/cloudcity/networking/CloudCityHelpers.java
@@ -12,10 +12,8 @@ import retrofit2.Retrofit;
 public class CloudCityHelpers {
     public static final String TAG = CloudCityHelpers.class.getSimpleName();
 
-    private static final String X_MAC_ADDRESS_HEADER = "X-MAC-Address ";
-
-    public static boolean sendData(String address, String token, NetworkDataModelRequest data) {
-        Log.d(TAG, "--> sendData()\taddress=" + address + ", token=" + token + ", data=" + data);
+    public static boolean sendData(String address, String MACaddr, NetworkDataModelRequest data) {
+        Log.d(TAG, "--> sendData()\taddress=" + address + ", MACaddr=" + MACaddr + ", data=" + data);
         Retrofit retrofit = NetworkClient.getRetrofitClient(address);
         if (retrofit == null) {
             Log.e(TAG, "sendData: Retrofit not set, exiting.");
@@ -26,7 +24,7 @@ public class CloudCityHelpers {
 
         try {
             Log.d(TAG, String.format(Locale.US, "sendData: Executing send data request: address %s", address));
-            response = api.sendData(X_MAC_ADDRESS_HEADER + token, data).execute();
+            response = api.sendData(MACaddr, data).execute();
             Log.d(TAG, String.format(Locale.US, "Received sendData response. %s", response));
 
             if (response.isSuccessful()) {

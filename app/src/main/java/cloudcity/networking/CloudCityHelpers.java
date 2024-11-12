fe58a1fd55af98649ab9c0af08743174611dc90a
RkShaRkz <darksidemetatron@gmail.com>
2024-11-12 14:43:22 +0100

    Add data validation before sending to server
    In case the data's latitude and longitude are 0, we now consider that
    non-valid data, and will not send it to the server.

--- app/src/main/java/cloudcity/networking/CloudCityHelpers.java
+++ app/src/main/java/cloudcity/networking/CloudCityHelpers.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.util.Locale;
 
+import cloudcity.networking.models.NetworkDataModel;
 import cloudcity.networking.models.NetworkDataModelRequest;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -23,20 +24,37 @@ public class CloudCityHelpers {
         Response<Void> response = null;
 
         try {
-            Log.d(TAG, String.format(Locale.US, "sendData: Executing send data request: address %s", address));
-            response = api.sendData("Bearer " + token, data).execute();
-            Log.d(TAG, String.format(Locale.US, "Received sendData response. %s", response));
+            // Check if data has valid Lat,Lng pair, do not send if they are (0,0)
+            boolean continueSending = validateData(data);
+            if (continueSending) {
+                Log.d(TAG, String.format(Locale.US, "sendData: Executing send data request: address %s", address));
+                response = api.sendData("Bearer " + token, data).execute();
+                Log.d(TAG, String.format(Locale.US, "Received sendData response. %s", response));
 
-            if (response.isSuccessful()) {
-                return true;
+                if (response.isSuccessful()) {
+                    return true;
+                } else {
+                    Log.d(TAG, "sendData: Send data request failed.");
+                }
+                return false;
             } else {
-                Log.d(TAG, "sendData: Send data request failed.");
+                Log.e(TAG, "sendData: data's location information was (0,0) - skipping sending!");
+                return false;
             }
-            return false;
-
         } catch (IOException e) {
             Log.e(TAG, "sendData: Failure to receive response.", e);
             return false;
         }
+    }
+
+    private static boolean validateData(NetworkDataModelRequest data) {
+        // Assume data is valid
+        boolean isValid = true;
+        // We have valid data if lattitude and longitude are non-zero
+        for (NetworkDataModel model : data.getData()) {
+            isValid = isValid && model.hasNonZeroLatitudeAndLongitude();
+        }
+
+        return isValid;
     }
 }

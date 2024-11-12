4c56433c163b9662e28c1e5a530e13a16ba83c55
RkShaRkz <darksidemetatron@gmail.com>
2024-11-12 15:15:36 +0100

    Do not send data to server if lat lng are zero (#36)
    * Add data validation before sending to server
    
    In case the data's latitude and longitude are 0, we now consider that
    non-valid data, and will not send it to the server.
    
    * Extend NetworkDataModel with a hasNonZeroLatLng() method
    
    * Address coderabbit's nitpicks

--- app/src/main/java/cloudcity/networking/models/NetworkDataModel.java
+++ app/src/main/java/cloudcity/networking/models/NetworkDataModel.java
@@ -6,6 +6,8 @@ import com.google.gson.annotations.SerializedName;
  * Base class for our network data
  */
 public class NetworkDataModel {
+    private static final double EPSILON = 0.00001;
+
     @SerializedName("lat")
     protected final double latitude;
     @SerializedName("lon")
@@ -20,4 +22,15 @@ public class NetworkDataModel {
     }
 
     static class NetworkDataModelValues { }
+
+    public boolean hasNonZeroLatitudeAndLongitude() {
+        return isNonZero(latitude) && isNonZero(longitude);
+    }
+
+    private boolean isNonZero(double value) {
+        // if we have a zero, or some very small number e.g. 0.00000000234, subtracting EPSILON
+        // from it and performing an abs() on the result will make it a number less than EPSILON
+        // since effectivelly it'd be as if we performed (EPSILON - small number)
+        return value != 0f && value != 0.0 && Math.abs(value - EPSILON) <= EPSILON;
+    }
 }

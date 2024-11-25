9353499375e7c43b438ca5331f941b9911f81892
RkShaRkz <darksidemetatron@gmail.com>
2024-11-25 14:53:13 +0100

    PingMonitor update; might be ready for an initial testrun
    Should be buildable, the start method is there... lets see where it breaks now.

--- app/src/main/java/cloudcity/PingMonitor.java
+++ app/src/main/java/cloudcity/PingMonitor.java
@@ -4,12 +4,25 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.HandlerThread;
 
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import cloudcity.dataholders.Iperf3MetricsPOJO;
 import cloudcity.util.CloudCityLogger;
-import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Iperf3.Iperf3Parser;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Metric.Metric;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Ping.PingInformations.PacketLossLine;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Ping.PingInformations.PingInformation;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Ping.PingInformations.RTTLine;
 import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Ping.PingParser;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.Ping.PingWorker;
 
 /**
  * The new single-source for programatically starting ping tests
@@ -19,6 +32,10 @@ public class PingMonitor {
 
     private final static long PARSING_DELAY_IN_MS = 10L; //0.01sec, was 1000L
 
+    private final static long PING_DELAY_IN_MS = 200L;
+
+    private final static String DEFAULT_PING_ADDRESS = "8.8.8.8";
+
     private Handler handler;
 
     private HandlerThread handlerThread;
@@ -27,16 +44,22 @@ public class PingMonitor {
 
     private Context appContext;
 
+    private Metric rttMetric;
+
+    private Metric packetLossMetric;
+
     private final AtomicBoolean shouldStop = new AtomicBoolean(false);
 
     private volatile PingParser pingParser;
+
+    private final ConcurrentSkipListSet<OneTimeWorkRequest> pingWRs;
 
     private final AtomicBoolean pingTestRunning = new AtomicBoolean(false);
 
     private volatile MainThreadExecutor mainThreadExecutor;
 
     /**
-     * Runnable used for parsing Iperf3 tests by constantly calling {@link #pingParser}'s
+     * Runnable used for parsing ping tests by constantly calling {@link #pingParser}'s
      * {@link PingParser#parse()} method every {@link #PARSING_DELAY_IN_MS} until it reaches the end
      * or ordered to stop by toggling {@link #shouldStop} flag
      */
@@ -58,6 +81,7 @@ public class PingMonitor {
 
     private PingMonitor() {
         // private constructor to prevent instantiation
+        pingWRs = new ConcurrentSkipListSet<>();
     }
 
     /**
@@ -74,7 +98,7 @@ public class PingMonitor {
         } else {
             // Since instance is null, this branch is already the first part of
             // the double-locked synchronized singleton creation
-            synchronized (Iperf3Monitor.class) {
+            synchronized (PingMonitor.class) {
                 if (instance == null) {
                     instance = new PingMonitor();
                 }
@@ -85,6 +109,11 @@ public class PingMonitor {
         instance.handlerThread.start();
         instance.handler = new Handler(instance.handlerThread.getLooper());
         instance.appContext = appContext;
+        instance.pingWRs.clear();
+
+        //TODO reconsider this bit, as it will collide with regular pinger
+        // And clear up all occurances of work
+//        wm.cancelAllWorkByTag("Ping");
     }
 
     /**
@@ -135,11 +164,38 @@ public class PingMonitor {
         }
     }
 
-    public void startPingTest() {}
+    public void startPingTest(PingMonitorCompletionListener completionListener) {
+        CloudCityLogger.d(TAG, "--> startPingTest()\tlistener: "+completionListener+"\tshouldStop: " + shouldStop.get());
+        shouldStop.set(false);
 
-    private void startParsingThread(Iperf3Parser newIperf3Parser) {
+        pingParser = PingParser.getInstance(null);
+        CloudCityLogger.v(TAG, "Adding property change listener");
+        pingParser.addPropertyChangeListener(evt -> {
+            PingInformation pi = (PingInformation) evt.getNewValue();
+            switch (pi.getLineType()) {
+                case RTT:
+                    rttMetric.update(((RTTLine) pi).getRtt());
+                    break;
+                case PACKET_LOSS:
+                    packetLossMetric.update(((PacketLossLine) pi).getPacketLoss());
+                    //packetLossMetric.setVisibility(View.VISIBLE);
+                    break;
+            }
+        });
+
+        startPingThread();
+        startParsingThread(pingParser);
+    }
+
+    private void startPingThread() {
+        CloudCityLogger.d(TAG, "--> startPingThread()");
+        handler.post(pingUpdate);
+        CloudCityLogger.d(TAG, "<-- startPingThread()");
+    }
+
+    private void startParsingThread(PingParser newPingParser) {
         CloudCityLogger.d(TAG, "--> startParsingThread()");
-        iperf3Parser = newIperf3Parser;
+        pingParser = newPingParser;
         handler.post(parsingRunnable);
         CloudCityLogger.d(TAG, "<-- startParsingThread()");
     }
@@ -148,21 +204,70 @@ public class PingMonitor {
         handler.removeCallbacks(parsingRunnable);
     }
 
+    private final Runnable pingUpdate = new Runnable() {
+        @Override
+        public void run() {
+            Data data = new Data.Builder()
+                    .putString("input", DEFAULT_PING_ADDRESS)
+                    .build();
+            OneTimeWorkRequest pingWR =
+                    new OneTimeWorkRequest.Builder(PingWorker.class)
+                            .setInputData(data)
+                            .addTag("Ping").build();
+            pingWRs.add(pingWR);
+
+            getWorkManager().beginWith(pingWR).enqueue();
+            Observer observer = new Observer() {
+                @Override
+                public void onChanged(Object o) {
+                    WorkInfo workInfo = (WorkInfo) o;
+                    WorkInfo.State state = workInfo.getState();
+                    switch (state){
+                        case RUNNING:
+                        case ENQUEUED:
+                            return;
+                        case CANCELLED:
+                            try {
+                                //TODO see if we need this stream at all
+                                FileOutputStream ping_stream = new FileOutputStream("logfile");
+                                ping_stream.close();
+                            } catch (IOException e) {
+                                CloudCityLogger.e(TAG,e.toString(), e);
+                            }
+                            handler.removeCallbacks(pingUpdate);
+                            return;
+                    }
+
+                    getWorkManager().getWorkInfoByIdLiveData(pingWR.getId()).removeObserver(this);
+                    pingWRs.remove(pingWR);
+                    handler.postDelayed(pingUpdate, PING_DELAY_IN_MS);
+                }
+            };
+            // We cannot observe on a background thread, so lets do the same hack as in Iperf3Monitor
+            MainThreadExecutor mainThreadExecutor = MainThreadExecutor.getInstance();
+            mainThreadExecutor.execute(() -> getWorkManager().getWorkInfoByIdLiveData(pingWR.getId()).observeForever(observer));
+        }
+    };
+
+    private WorkManager getWorkManager() {
+        return WorkManager.getInstance(appContext);
+    }
+
     private void calculateAndLogMetrics() {
-        double DLmin = normalize(defaultReverseThroughput.calcMin());
-        double DLmedian = normalize(defaultReverseThroughput.calcMedian());
-        double DLmax = normalize(defaultReverseThroughput.calcMax());
-        double DLmean = normalize(defaultReverseThroughput.calcMean());
-        double DLlast = normalize(getLast(defaultReverseThroughput.getMeanList()));
-
-        double ULmin = normalize(defaultThroughput.calcMin());
-        double ULmedian = normalize(defaultThroughput.calcMedian());
-        double ULmax = normalize(defaultThroughput.calcMax());
-        double ULmean = normalize(defaultThroughput.calcMean());
-        double ULlast = normalize(getLast(defaultThroughput.getMeanList()));
-
-        CloudCityLogger.d(TAG, "download speeds: MIN=" + DLmin + ", MED=" + DLmedian + ", MAX=" + DLmax + ", MEAN=" + DLmean + ", LAST=" + DLlast);
-        CloudCityLogger.d(TAG, "upload speeds: MIN=" + ULmin + ", MED=" + ULmedian + ", MAX=" + ULmax + ", MEAN=" + ULmean + ", LAST=" + ULlast);
+//        double DLmin = normalize(defaultReverseThroughput.calcMin());
+//        double DLmedian = normalize(defaultReverseThroughput.calcMedian());
+//        double DLmax = normalize(defaultReverseThroughput.calcMax());
+//        double DLmean = normalize(defaultReverseThroughput.calcMean());
+//        double DLlast = normalize(getLast(defaultReverseThroughput.getMeanList()));
+//
+//        double ULmin = normalize(defaultThroughput.calcMin());
+//        double ULmedian = normalize(defaultThroughput.calcMedian());
+//        double ULmax = normalize(defaultThroughput.calcMax());
+//        double ULmean = normalize(defaultThroughput.calcMean());
+//        double ULlast = normalize(getLast(defaultThroughput.getMeanList()));
+//
+//        CloudCityLogger.d(TAG, "download speeds: MIN=" + DLmin + ", MED=" + DLmedian + ", MAX=" + DLmax + ", MEAN=" + DLmean + ", LAST=" + DLlast);
+//        CloudCityLogger.d(TAG, "upload speeds: MIN=" + ULmin + ", MED=" + ULmedian + ", MAX=" + ULmax + ", MEAN=" + ULmean + ", LAST=" + ULlast);
     }
 
     /**
